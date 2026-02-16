6b7568e9b4a2e2727045c12f9cc796d2a2b5479c
Hashir <hashir.ali@dubizzlelabs.com>
2026-02-16 19:15:38 +0500

    added validation adding Form schema when creating request type

--- app/Http/Requests/StoreRequestTypeRequest.php
+++ app/Http/Requests/StoreRequestTypeRequest.php
@@ -19,7 +19,33 @@ class StoreRequestTypeRequest extends FormRequest
         return [
             'name' => 'required|string|unique:request_types,name',
             'description' => 'nullable|string',
-            'form_schema' => 'required|array', // JSON/Array of validation rules
+            'form_schema' => [
+                'required',
+                'array',
+                function ($attribute, $value, $fail) {
+                    foreach (array_keys($value) as $key) {
+                        if (!preg_match('/^[a-z][a-z0-9_]*$/i', $key)) {
+                            $fail("The field name \"$key\" is invalid. Use letters, numbers and underscores, starting with a letter.");
+                        }
+                    }
+                }
+            ],
+            'form_schema.*' => [
+                'required',
+                'string',
+                function ($attribute, $value, $fail) {
+                    try {
+                        // Dry-run the rule syntax using a dummy validator
+                        $validator = \Illuminate\Support\Facades\Validator::make(
+                            ['test' => 'value'],
+                            ['test' => $value]
+                        );
+                        $validator->passes();
+                    } catch (\Exception $e) {
+                        $fail("The validation rule \"$value\" is invalid or contains syntax errors.");
+                    }
+                }
+            ],
         ];
     }
 }
--- app/Http/Requests/UpdateRequestTypeRequest.php
+++ app/Http/Requests/UpdateRequestTypeRequest.php
@@ -19,7 +19,34 @@ class UpdateRequestTypeRequest extends FormRequest
         return [
             'name' => 'required|string|unique:request_types,name,' . $this->route('request_type'),
             'description' => 'nullable|string',
-            'form_schema' => 'nullable|array',
+            'form_schema' => [
+                'nullable',
+                'array',
+                function ($attribute, $value, $fail) {
+                    if (is_array($value)) {
+                        foreach (array_keys($value) as $key) {
+                            if (!preg_match('/^[a-z][a-z0-9_]*$/i', $key)) {
+                                $fail("The field name \"$key\" is invalid. Use letters, numbers and underscores, starting with a letter.");
+                            }
+                        }
+                    }
+                }
+            ],
+            'form_schema.*' => [
+                'required',
+                'string',
+                function ($attribute, $value, $fail) {
+                    try {
+                        $validator = \Illuminate\Support\Facades\Validator::make(
+                            ['test' => 'value'],
+                            ['test' => $value]
+                        );
+                        $validator->passes();
+                    } catch (\Exception $e) {
+                        $fail("The validation rule \"$value\" is invalid or contains syntax errors.");
+                    }
+                }
+            ],
         ];
     }
 }
